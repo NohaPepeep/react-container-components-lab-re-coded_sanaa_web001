d8ab1cd5c61b5fbf334ca5c7edaa2a230de9fbe2
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-09-22 23:16:53 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,25 +1,14 @@
-
 import React from 'react';
 
-const Review = ({headline,byline,link,summary_short}) => {
-  return (
+const MovieReviews = (props) =>
+  <div className="review-list">
+      {props.reviews.map(movie => {
+          return <div className="review" key={movie.display_title}>
+                <a href={movie.link.url}>{movie.headline}</a>
+                <p>By: {movie.byline}</p>
+                <hr></hr>
+            </div>
+      })}
+  </div>
 
-    <div key={headline} className="review">
-      <header>
-        <a className="review-link" href={link.url}>
-          {headline}
-        </a>
-        <span className="author">{byline}</span>
-      </header>
-      <blockquote>{summary_short}</blockquote>
-    </div>
-  );
-};
-
-const MovieReviews = ({ reviews }) => <div className="review-list">{reviews.map(Review)}</div>;
-
-MovieReviews.defaultProps = {
-  reviews: []
-};
-
-export default MovieReviews;
+export default MovieReviews 
