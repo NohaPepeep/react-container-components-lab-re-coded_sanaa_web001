bab211867c0846f54eb72bac8a38e798ac7bb73f
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-09-22 23:18:16 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MovieReviews = (props) =>
   <div className="review-list">
       {props.reviews.map(movie => {
-          return <div className="review" key={movie.display_title}>
+          return <div className="review">
                 <a href={movie.link.url}>{movie.headline}</a>
                 {movie.byline}
             </div>
