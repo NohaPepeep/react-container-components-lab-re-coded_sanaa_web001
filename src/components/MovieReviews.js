eb1f37c9ce977456fdf4e0ebbdf732decbd4ceae
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-09-22 23:11:44 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,22 +1,14 @@
 
 import React from 'react';
 
-const Review = ({
-  headline,
-  byline,
-  link,
-  summary_short
-}) => {
+const Review = ({headline,byline,link,summary_short}) => {
   return (
 
     <div key={headline} className="review">
       <header>
-        <a className="review-link"
-          href={link.url}
-        >
+        <a className="review-link" href={link.url}>
           {headline}
         </a>
-        <br/>
         <span className="author">{byline}</span>
       </header>
       <blockquote>{summary_short}</blockquote>
