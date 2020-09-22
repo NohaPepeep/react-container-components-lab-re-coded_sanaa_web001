ff2fa6348536a99f1510e5eecee63465b7b66377
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-09-22 23:10:38 +0000

    Automatically backed up by Learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -9,13 +9,9 @@ const Review = ({
 }) => {
   return (
 
-    <div
-      key={headline}
-      className="review"
-    >
+    <div key={headline} className="review">
       <header>
-        <a
-          className="review-link"
+        <a className="review-link"
           href={link.url}
         >
           {headline}
